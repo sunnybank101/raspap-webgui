cdadf30fe2efbd15dcf9edfd5e038ab7d02306fe
sunnybank101 <52540694+sunnybank101@users.noreply.github.com>
2019-07-04 17:36:55 +0100

    static setting does not work

--- ajax/networking/gen_int_config.php
+++ ajax/networking/gen_int_config.php
@@ -8,26 +8,32 @@ if(isset($_POST['generate']) && isset($_POST['csrf_token']) && CSRFValidate()) {
     $cnfNetworking = array_combine($cnfNetworking,$cnfNetworking);
     $strConfFile = "";
     foreach($cnfNetworking as $index=>$file) {
-        if($index != "defaults") {
-            $cnfFile = parse_ini_file(RASPI_CONFIG_NETWORKING.'/'.$file);
-            if($cnfFile['static'] === 'true') {
-                $strConfFile .= "interface ".$cnfFile['interface']."\n";
-                $strConfFile .= "static ip_address=".$cnfFile['ip_address']."\n";
-                $strConfFile .= "static routers=".$cnfFile['routers']."\n";
-                $strConfFile .= "static domain_name_servers=".$cnfFile['domain_name_server']."\n";
-            } elseif($cnfFile['static'] === 'false' && $cnfFile['failover'] === 'true') {
-                $strConfFile .= "profile static_".$cnfFile['interface']."\n";
-                $strConfFile .= "static ip_address=".$cnfFile['ip_address']."\n";
-                $strConfFile .= "static routers=".$cnfFile['routers']."\n";
-                $strConfFile .= "static domain_name_servers=".$cnfFile['domain_name_server']."\n\n";
-                $strConfFile .= "interface ".$cnfFile['interface']."\n";
-                $strConfFile .= "fallback static_".$cnfFile['interface']."\n\n";
-            } else {
-                $strConfFile .= "#DHCP configured for ".$cnfFile['interface']."\n\n";
-            }
-        } else {
-            $strConfFile .= file_get_contents(RASPI_CONFIG_NETWORKING.'/'.$index)."\n\n";
-        }
+		
+		if($index != "defaults") {
+			if(substr($index,-4) === ".ini") { // only process ini files
+				$cnfFile = parse_ini_file(RASPI_CONFIG_NETWORKING.'/'.$file);
+				if($cnfFile['static'] === '1') { // true is '1' once loaded
+					$strConfFile .= "\n#STATIC configured for [".$cnfFile['interface']."]\n\n";
+					$strConfFile .= "interface ".$cnfFile['interface']."\n";
+					$strConfFile .= "static ip_address=".$cnfFile['ip_address']."\n";
+					$strConfFile .= "static routers=".$cnfFile['routers']."\n";
+					$strConfFile .= "static domain_name_servers=".$cnfFile['domain_name_server']."\n";
+				} elseif($cnfFile['static'] === '0' && $cnfFile['failover'] === '1') { // true is '1' once loaded
+					$strConfFile .= "\n#STATIC FAILOVER configured for ".$cnfFile['interface']."\n\n";
+					$strConfFile .= "profile static_".$cnfFile['interface']."\n";
+					$strConfFile .= "static ip_address=".$cnfFile['ip_address']."\n";
+					$strConfFile .= "static routers=".$cnfFile['routers']."\n";
+					$strConfFile .= "static domain_name_servers=".$cnfFile['domain_name_server']."\n\n";
+					$strConfFile .= "interface ".$cnfFile['interface']."\n";
+					$strConfFile .= "fallback static_".$cnfFile['interface']."\n\n";
+				} else {
+					$strConfFile .= "\n#DHCP configured for [".$cnfFile['interface']."]\n\n";
+				}
+			}
+		} else {
+			$strConfFile .= file_get_contents(RASPI_CONFIG_NETWORKING.'/'.$index)."\n\n";
+		}
+		
     }
 
     if(file_put_contents(RASPI_CONFIG_NETWORKING.'/dhcpcd.conf',$strConfFile)) {
