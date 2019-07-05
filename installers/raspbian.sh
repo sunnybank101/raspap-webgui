17d21e3f5c866f785f6e852f7b3b5b73e167af87
sunnybank101 <52540694+sunnybank101@users.noreply.github.com>
2019-07-05 10:58:25 +0100

    THROW AWAY - install from sunnybank101 branch

--- installers/raspbian.sh
+++ installers/raspbian.sh
@@ -1,4 +1,6 @@
-UPDATE_URL="https://raw.githubusercontent.com/billz/raspap-webgui/master/"
+#UPDATE_URL="https://raw.githubusercontent.com/billz/raspap-webgui/master/"
+UPDATE_URL="https://raw.githubusercontent.com/sunnybank101/raspap-webgui/sunnybank101-multiinterfaces/"
+
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/raspapcommon.sh
 source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
 
